13795f85ee5036d03d6bb34fe16bd6090d6925cd
gabriel0918 <120161291+gabriel0918@users.noreply.github.com>
2022-12-09 22:00:22 +0900

    #1 이거 왜 올라갔을까요 ?
    재복.. 테스트중

--- controller/sign_route.py
+++ controller/sign_route.py
@@ -1,5 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from flask import render_template
+from pymongo import MongoClient
+
 from . import routes
+load_dotenv()
+mySecretKey = os.environ.get('MySecretKey')
+client = MongoClient(mySecretKey)
+db = client.worldcup
 
 @routes.route('/sign', methods=['GET'])
 def sign_get():
