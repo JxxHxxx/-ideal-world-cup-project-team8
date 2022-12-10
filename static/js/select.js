7c82885db6de0cb8b7fc0f877f6c9db3c40eac01
JxxHxxx <leesin5498@naver.com>
2022-12-10 14:43:08 +0900

    FEAT #3 N강 설정 기능, 이미지 크기 조절, 콘솔 로그 삭제
    N강 설정 기능
    select_config.js
    round_type 전역 변수를 통해 몇 강을 진행할 것인지 선택할 수 있도록 합니다.

--- static/js/select.js
+++ static/js/select.js
@@ -8,7 +8,8 @@ let now_round = 0;
 let candidate_images_index = 0;
 let next_round_images_index = 0;
 
-let final = 0;
+let total_matches = 0;
+
 
 function read_result() {
     $.ajax({
@@ -39,7 +40,7 @@ function add_noodle() {
        success: function (response) {
            ideal_list = response['ideal']
 
-           for (let i = 0; i < 4; i++) {
+           for (let i = 0; i < round_type[0]; i++) {
                 candidate_images[i] = ideal_list[i]['img']
            }
            pick()
@@ -53,29 +54,28 @@ function pick() {
 }
 
 function change(idx) {
-    if (final < 4) {
-        final++
+    if (total_matches < round_type[0]) {
+        total_matches++
         now_round_total_matches = candidate_images.length/2
-        console.log("Round " + candidate_images_index/2)
-        console.log("이번 " +candidate_images.length + "강의 매치 횟수 " + now_round_total_matches)
+
         now_round++
 
         if (idx == 0) {
-            console.log("왼쪽을 선택하셨습니다.")
+
             next_round_images[next_round_images_index++] = candidate_images[candidate_images_index]
         } else {
-            console.log("오른쪽을 선택하셨습니다.")
+
             next_round_images[next_round_images_index++] = candidate_images[candidate_images_index + 1]
         }
 
         if (now_round_total_matches >= 1) {
             candidate_images_index = candidate_images_index + 2
-            console.log("다음 경기를 준비합니다.")
+
             pick()
         }
 
         if (now_round_total_matches == now_round) {
-            console.log(candidate_images.length + "강 매치가 끝났습니다.")
+
             candidate_images = []
 
             for (let i = 0; i < next_round_images.length; i++) {
@@ -83,17 +83,15 @@ function change(idx) {
             }
 
             if (next_round_images.length != 1) {
+
                 next_round_images = []
                 candidate_images_index = 0
                 next_round_images_index = 0
                 now_round = 0
-                console.log("다음 매치를 위한 초기화 작업이 종료되었습니다.")
                 pick()
             } else {
                 document.getElementById("first_image").src = next_round_images[0]
-                //최종 결과는 한장으로 나오게
 
-                // let result_html = `<button onclick="send_result()">결과 보기</button>`
                 let result_html = `<button onclick="location.href='select/result'; save_result()">결과 보기</button>`
                 $('#result').append(result_html)
 
