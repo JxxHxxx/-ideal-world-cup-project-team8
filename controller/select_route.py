2f591a1f1fb57b48468851d8a769b2fb2a363c7c
JxxHxxx <leesin5498@naver.com>
2022-12-12 16:56:01 +0900

    FEAT #6 member 인증, 투표 결과 상세 내역 API 구현

--- controller/select_route.py
+++ controller/select_route.py
@@ -6,15 +6,11 @@ import jwt
 from pymongo import MongoClient
 
 from . import routes
-
+SECRET_KEY = 'SPARTA'
 load_dotenv()
 mySecretKey = os.environ.get('MySecretKey')
 client = MongoClient(mySecretKey)
 db = client.worldcup
-
-@routes.route('/select', methods=['GET'])
-def select():
-    return render_template('idealSelect.html')
 
 @routes.route('/api/noodle/read', methods=['GET'])
 def read_noddle():
@@ -38,15 +34,59 @@ def get_cookies():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("routes.get_cookies", msg="로그인 정보가 존재하지 않습니다."))
 
-@routes.route('/select/result', methods=['GET'])
-def result():
-    return render_template('result.html')
-
 @routes.route('/api/noodle/save', methods=['POST'])
 def save_result():
+    token = request.cookies.get("mytoken")
+    payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=['HS256'])
+    login_member = db.member.find_one({'id': payload['id']})
+
     img_receive = request.form['img_give']
     find_noodle = db.noodle.find_one({'img': img_receive})
 
     db.noodle.update_one({'img': img_receive}, {'$set': {'win': find_noodle['win'] + 1}})
     db.noodle.update_one({'img': img_receive}, {'$push':{'nicknames': login_member['nickname']}})
     return "ok"
+
+@routes.route('/select/result', methods=['GET'])
+def result():
+    token = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        db.member.find_one({'id': payload['id']})
+        return render_template('result.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("routes.home", msg="로그인 시간이 만료되었습니다."))
+
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("routes.home", msg="로그인 정보가 존재하지 않습니다."))
+
+@routes.route('/select', methods=['GET'])
+def select():
+    token = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        db.member.find_one({'id': payload['id']})
+        return render_template('idealSelect.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("routes.home", msg="로그인 시간이 만료되었습니다."))
+
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("routes.home", msg="로그인 정보가 존재하지 않습니다."))
+
+@routes.route('/select/detail/<noodle_name>', methods=['GET'])
+def detail(noodle_name):
+    find_noodle = db.noodle.find_one({'name': noodle_name})
+    find_noodle['nicknames']
+    token = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        db.member.find_one({'id': payload['id']})
+        return render_template('detail.html', name=find_noodle['nicknames'])
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("routes.home", msg="로그인 시간이 만료되었습니다."))
+
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("routes.home", msg="로그인 정보가 존재하지 않습니다."))
+
+
+
